0e1e71be8adcfc9710449de11580fcca529da356
Carlos Aparicio <apariciovivero>
2021-11-07 16:31:36 -0500

    Creación de proyecto base mvc con spring

---
+++ src/main/java/com/mies/core/MiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mies.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mies/core/MiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mies.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
